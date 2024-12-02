93dc203ead7567e5f82ba92282c53bb2b627659a
alihuzaifa <ha4587323@gmail.com>
2024-12-02 18:39:15 +0500

    intro to javascript with variable and alert has covered in class

---
+++ JS/1) Intro Variables & Alert/app.js
@@ -0,0 +1,59 @@
+// alert(abcd);
+
+// Data Types 7
+// String =>   every characters except number '' "" 
+// Number =>   every number except characters and symbols
+
+
+
+// alert("Hello World");
+// alert(2222);
+
+
+// alert("Ali Huzaifa");
+// alert("Ali Huzaifa");
+// alert("Ali Huzaifa");
+// alert("Ali Huzaifa");
+// alert("Ali Huzaifa");
+// alert("Ali Huzaifa");
+// alert("Ali Huzaifa");
+// alert("Ali Huzaifa");
+// alert("Ali Huzaifa");
+
+
+// var name = "Ali Huzaifa";
+// alert(name);
+// alert(name);
+// alert(name);
+// alert(name);
+// alert(name);
+// alert(name);
+// alert(name);
+// alert(name);
+// alert(name);
+
+
+
+// var age = 22;
+// alert(age);
+
+
+// alert(1020828208 / 2682682628);
+
+
+
+// var number1 = "10";
+// var number2 = "20";
+// alert(number1 + number2);
+
+// var firstName = "Ali ";
+// var secondName = "Huzaifa";
+// alert(firstName + secondName);
+
+// Concatenate
+
+
+
+
+alert("Ali \n\n\n\n Huzaifa");
+
