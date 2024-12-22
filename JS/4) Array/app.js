eafb6ea2d20abbd67a6012be7eb6064eacb1e6b8
alihuzaifa <ha4587323@gmail.com>
2024-12-22 16:53:45 +0500

    array methods has covered

---
+++ JS/4) Array/app.js
@@ -0,0 +1,290 @@
+// console.log("hello world!");
+
+// data types
+
+// string "dsdasd" 'sadsadas'
+// number 123213.34234
+// boolean true | false
+
+// var username = true;
+// console.log(typeof(username))
+
+
+
+
+
+// function greetUser() {
+//     console.log("hello world");
+//     console.log("hello world");
+//     console.log("hello world");
+//     console.log("hello world");
+//     console.log("hello world");
+// }
+
+
+// greetUser()
+// greetUser()
+
+
+
+// function sum(num1 , num2){ // parameter receive
+//     console.log(num1 + num2)
+//     return num1 + num2
+// }
+
+
+// sum(10 , 20) // argument send
+// sum(40 , 50) // argument send
+
+// var total = sum(50 , 50)
+// console.log(sum(20 , 30));
+// console.log(total);
+
+
+// undefined vs null
+
+// null == kuch nahi
+// undefined == function recall nahi kiya
+
+
+
+// function greetUser(username , email){
+//     return 'your name is ' + username + " and my email is " + email
+// }
+
+// var greetings = greetUser("abdullah")
+
+// console.log(greetings);
+
+
+
+// concatination
+// console.log("20" + "20");
+
+
+
+
+
+// equal to
+// console.log(10 == "10")
+// console.log(10 === "10")
+
+
+// "=" assignment operator
+// "=="
+// "==="
+
+
+// NaN
+
+// var chemMarks = +prompt("enter chem marks");
+// var phyMarks = +prompt("enter phy marks");
+// var pstMarks = +prompt("enter pst marks");
+// var urduMarks = +prompt("enter urdu marks");
+
+// var total = chemMarks + phyMarks + pstMarks + urduMarks
+
+// console.log(total);
+
+
+
+
+
+
+//operators
+
+//Arithmatic Operator
+
+// +
+// -
+// *
+// /
+// % == modulus
+
+// console.log(2 + 2);
+
+// comparision operator
+
+// > greater than
+// < less than
+// == equals to
+// === double equals
+// != 
+// !== 
+// >= greater than and equals to
+// <= less than and equals to
+
+// console.log(2 == '2')
+// console.log(2 === '2')
+
+
+// logical operator
+
+// && == and urdu wala orr
+// || == OR urdu wala yaa
+
+// true && true
+// true
+
+// true && true
+// console.log(true || false);
+
+// false
+
+// var bankBalance = 100000
+
+// if (bankBalance > 1000000) {
+//     console.log("gaari ayegi");
+// }
+// else if (bankBalance > 500000) {
+//     console.log("shayad mehran ajaye");
+
+// } else {
+//     console.log("nahi ayegi");
+
+// }
+
+
+
+// console.log("if ka bhar");
+
+
+
+
+
+
+// var username = prompt("enter username")
+// console.log(username);
+
+
+
+// function sum(num1, num2) {
+//     console.log("sum of number ==> ", num1 + num2);
+
+//     return num1 + num2
+// }
+
+
+// var total = sum(20, 30)
+// var totalNum = sum(60 ,80)
+// console.log(total);
+// console.log(totalNum);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Array
+
+// var username = ["abdullah" , "usman" , "ammar"] 
+// console.log(username)
+
+// index
+// array ka index 0 sa start hota ha
+
+// length
+// array k length 1 sa start hoti ha
+
+
+//push || array ka end ma add krdeta ha
+//pop || array ka end ma remove krdeta ha
+//unshift || array ka start ma add krdeta ha
+//shift || array ka start ma remove krdeta ha
+//splice || beech ma sa ya kahi sa bhi add or delete krskta hain
+//slice || array ki shallow copy banata ha.
+//split || string ko array ma convert krta ha
+//join || array ko string ma convert krta haa
+//reverse || array ko reverse krdeta ha
+//concat
+//sort
+//indexof
+
+
+// var fruits = ["apple" , "mango" , "orange" , "banana"]
+// console.log(fruits[3]);
+
+// fruits.push("strawberry")
+// fruits.unshift("kiwi")
+
+// fruits.pop()
+// fruits.shift()
+
+
+// fruits.pop()
+// fruits.push("kiwi")
+
+
+// var fruits = ["apple", "mango", "orange", "banana", "grape", "pear", "kiwi", "pineapple", "strawberry", "blueberry", "watermelon", "peach", "plum", "cherry", "apricot"];
+
+
+// "strawberry", "blueberry", delete krna ha
+// "watermelon", "peach", "plum", "cherry" delete krka "hello world"
+//  "apple", "mango" delete krka yahaa "hello" " world"
+
+
+// fruits.splice(8 , 2)
+// fruits.splice(10 , 4 , "hello world")
+// fruits.splice(0 , 2 , "hello" , "world")
+// console.log(fruits);
+
+
+
+// starting index
+// how many item should delete
+// fruits.splice(2 , 1 , "strawberry")
+// fruits.splice(2 , 0 , "strawberry")
+// console.log(fruits)
+
+
+
+
+// methods or function aik hi cheez hain
+
+
+
+var fruits = ["apple", "mango", "orange", "banana", "grape", "pear", "kiwi", "pineapple", "strawberry", "blueberry", "watermelon", "peach", "plum", "cherry", "apricot"];
+
+
+// var newArr = fruits.slice(0 , 4)
+// var newArr = fruits.slice(3 , 9)
+// var newArr = fruits.slice(8)
+
+// var str = fruits.join("z")
+// console.log(str);
+
+
+
+// console.log(newArr);
+
+
+// var username = "Muhammad Abdullah Khan";
+
+// var arr = username.split("")
+// console.log(arr);
+
+
+
+// string ko reverse krdo
+
+// var city = "kiraanchi";
+// console.log(city.split("").reverse().join(""))
+
+
+
+
+
+
+
+
