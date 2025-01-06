e6c87269828cbdf55d90bb81e9fb9f34321516ee
alihuzaifa1995 <alihuzaifa1995@gmail.com>
2025-01-06 11:20:25 +0500

    String Number Methods has covered with dice game

---
+++ JS/6) String & Number Methods/app.js
@@ -0,0 +1,99 @@
+// var cities = ["KarAChi", "Lahore", "Islamabad", "Quetta"];
+// var userCity = prompt("Enter your city name?");
+
+// var isCityFind = false;
+
+// // Boolean true false
+
+// for (var i = 0; i < cities.length; i++) {
+//   var city = cities[i];
+//   if (userCity.toUpperCase() === city.toUpperCase()) {
+//     isCityFind = true;
+//     break;
+//   }
+// }
+
+// if (isCityFind === true) {
+//   console.log("City name is correct");
+// } else {
+//   console.log("City name is not correct");
+// }
+
+// var cityName = "Karachi";
+// console.log(cityName.toLowerCase());index
+// console.log(cityName.toUpperCase());
+
+// console.log("1");
+// console.log("2");
+// console.log("3");
+// console.log("4");
+// console.log("5");
+
+//
+// console.log("6 7 8 9 10");
+// console.log("11 12 13 14 15");
+
+// var count = 1;
+// for (let row = 1; row <= 5; row++) {
+//   var currentRow = "";
+//   for (var column = 1; column <= 5; column++) {
+//     currentRow += " " + count;
+//     count++;
+//   }
+
+//   console.log(currentRow);
+// }
+
+// A = A
+//  =  q
+// q = qu
+
+// the = THE
+// World War II =>
+
+// for (var index = 0; index < text.length; index++) {
+//   var copy = text.slice(index, index + 12);
+//   if (copy === "World War II") {
+//     var previousText = text.slice(0, index);
+//     var nextText = text.slice(index + 12);
+//     text = previousText + "Second World War" + nextText;
+//   }
+// }
+
+// console.log(text);
+
+// for (var index = 0; index < text.length; index++) {
+//   console.log(text[index]);
+// }
+
+// console.log(text.slice(0, 5));
+// console.log(text.slice(0, 2 + 3));
+
+// var text =
+//   "World War II was one of the most devastating conflicts in human history, fundamentally Huzaifa reshaping the global order. The causes of World War II were deeply rooted in the unresolved issues of World War II, including the harsh terms imposed by the Treaty of Versailles and the rise of totalitarian regimes in Germany, Italy, and Japan. As World War II spread across Europe and Asia, it became clear that the scale of destruction would exceed anything seen in earlier wars. The aftermath of World War II led to the creation of the United Nations, aimed at preventing another World War II. While the world hoped for peace, the impact of World War II continued to influence geopolitics for decades. Even today, the lessons of World War II remind us of the dangers of unchecked aggression, intolerance, and the Huzaifa failure to resolve international conflicts before they escalate into full-scale wars.";
+
+// // console.log(text.replace("World War II", "Second World War"));
+// // console.log(text.replace(/World War II/g, "Second World War"));
+// // console.log(text.indexOf("War"));
+// console.log(text.lastIndexOf("Huzaifa"));
+
+// var number = 10.9;
+
+//  .>=5    next number
+//  .<5    previous number
+// console.log(Math.round(number));
+// console.log(Math.ceil(number));
+// console.log(Math.floor(number));
+
+// var randomNumber = Math.ceil(Math.random() * 6);
+// console.log(randomNumber);
+
+var number = 100;
+// console.log(parseInt(number));
+// console.log(parseFloat(number));
+// console.log(Number(number));
+// console.log(Number(true));
+// console.log(Number(false));
+// console.log(Number(["Mango"]));
+// console.log(Number(["Mango"]));
+console.log(number.toFixed(5));
